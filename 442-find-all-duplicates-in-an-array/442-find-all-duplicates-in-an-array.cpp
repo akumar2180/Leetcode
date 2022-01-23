956971ee4a036a8b335d3c6f8049db75717a7d25
akumar2180 <71609081+akumar2180@users.noreply.github.com>
2022-01-23 15:32:44 +0530

    Time: 181 ms (6.48%), Space: 44.6 MB (16.13%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+         if(nums.empty())return {};
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int no:nums)mp[no]++;
+        for(auto it:mp)if(it.second==2)ans.push_back(it.first);
+        return ans;
+        
+
+ // seond method //
+        
+        
+        /* // unordered_map to store frequencies
+    unordered_map<int, int> freq;
+    for (int i=0; i<nums.size(); i++)
+        freq[nums[i]]++;
+ 
+    bool dup = false;
+    unordered_map<int, int>:: iterator itr;
+    for (itr=freq.begin(); itr!=freq.end(); itr++)
+    {
+        // if frequency is more than 1
+        // print the element
+        if (itr->second > 1)
+        {
+            return itr->first << " ";
+            
+        }
+    }
+ 
+    // no duplicates present
+    if (dup == false)
+        return -1;
+}
+*/
+      
+        
+    }
+};
