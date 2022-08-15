bada411d036632a16df1efea4a15d772faec4d9b
akumar2180 <71609081+akumar2180@users.noreply.github.com>
2022-08-15 19:09:48 +0530

    Time: 13 ms (39.79%), Space: 8.9 MB (32.98%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> ans;
-        stack<int> st;
+       unordered_map<int,int>umap;
+        stack<int>st;
         int n = nums2.size();
-        for (int i = n - 1; i >= 0; --i) {
-            int num = nums2[i];
-            while (!st.empty() && st.top() <= num)
+        
+        for(int i=n-1; i>=0; i--){
+            int ele = nums2[i];
+            while(!st.empty() && st.top() <= ele){
                 st.pop();
-            if (!st.empty())
-                ans[num] = st.top();
-            st.push(num);
+            }
+            int res = (st.empty()) ? -1 : st.top();
+            umap.insert({ele, res});
+            st.push(ele);
         }
-        vector<int> out;
-        for (int num : nums1) {
-            if (ans.count(num))
-                out.push_back(ans[num]);
-            else
-                out.push_back(-1);
+        
+        vector<int> ans;
+        for(auto x: nums1){
+            ans.push_back(umap[x]);
         }
-        return out;
+        
+        return ans;
         
       
     }
